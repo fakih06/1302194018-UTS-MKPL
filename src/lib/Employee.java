ac4adb9bdc4a39ce7de92d5c1a4f227b99a25948
madF <58264400+fakih06@users.noreply.github.com>
2022-04-20 23:20:03 +0800

    Revisi method
    merapihkan dan menambah class pada spouse

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -21,15 +21,9 @@ public class Employee {
 	private boolean isForeigner;
 	private boolean gender; //true = Laki-laki, false = Perempuan
 	
-<<<<<<< Updated upstream
-	// private int monthlySalary;
-	// private int otherMonthlyIncome;
-	// private int annualDeductible;
-	
+
 	private String spouseName;
 	private String spouseIdNumber;
-=======
->>>>>>> Stashed changes
 
 	public Employee(
 			String employeeId,
@@ -49,58 +43,16 @@ public class Employee {
 		this.dayJoined = dayJoined;
 		this.isForeigner = isForeigner;
 		this.gender = gender;
-		
-<<<<<<< Updated upstream
-		// childNames = new LinkedList<String>();
-		// childIdNumbers = new LinkedList<String>();
+
 	}
 	
-	// /**
-	//  * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	//  * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	//  */
-	
-	// public void setMonthlySalary(int grade) {	
-	// 	if (grade == 1) {
-	// 		monthlySalary = 3000000;
-	// 		if (isForeigner) {
-	// 			monthlySalary = (int) (3000000 * 1.5);
-	// 		}
-	// 	}else if (grade == 2) {
-	// 		monthlySalary = 5000000;
-	// 		if (isForeigner) {
-	// 			monthlySalary = (int) (3000000 * 1.5);
-	// 		}
-	// 	}else if (grade == 3) {
-	// 		monthlySalary = 7000000;
-	// 		if (isForeigner) {
-	// 			monthlySalary = (int) (3000000 * 1.5);
-	// 		}
-	// 	}
-	// }
-	
-	// public void setAnnualDeductible(int deductible) {	
-	// 	this.annualDeductible = deductible;
-	// }
-	
-	// public void setAdditionalIncome(int income) {	
-	// 	this.otherMonthlyIncome = income;
-	// }
 	
 	public void setSpouse(String spouseName, String spouseIdNumber) {
 		this.spouseName = spouseName;
 		this.spouseIdNumber = idNumber;
 	}
 	
-	// public void addChild(String childName, String childIdNumber) {
-	// 	childNames.add(childName);
-	// 	childIdNumbers.add(childIdNumber);
-	// }
-=======
 
-	}
->>>>>>> Stashed changes
-	
 	public int getAnnualIncomeTax() {
 		
 		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
