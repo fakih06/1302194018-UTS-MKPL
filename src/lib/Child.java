5a66f58e5f18d457f133f824eaf1cd069f08f160
madF <58264400+fakih06@users.noreply.github.com>
2022-04-20 23:31:50 +0800

    Update Child.java
    membuat class baru

--- src/lib/Child.java
+++ src/lib/Child.java
@@ -1,16 +1,27 @@
 package lib;
 
-public class Child extends Employee{
-   
-	private List<String> childNames;
-	private List<String> childIdNumbers;
+public class Child {
+    private String childName;
+    private String childIdNumbers;
 
-    public Child (){
-
+    public Child(String childName, String childIdNumbers) {
+        this.childName = childName;
+        this.childIdNumbers = childIdNumbers;
     }
 
-    public void addChild(String childName, String childIdNumber) {
-		childNames.add(childName);
-		childIdNumbers.add(childIdNumber);
-	}
+    public String getChildName() {
+        return childName;
+    }
+
+    public void setChildName(String childName) {
+        this.childName = childName;
+    }
+
+    public String getChildIdNumbers() {
+        return childIdNumbers;
+    }
+
+    public void setChildIdNumbers(String childIdNumbers) {
+        this.childIdNumbers = childIdNumbers;
+    }
 }
